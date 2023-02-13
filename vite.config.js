348d4ff4f50340472a38b3199d751c12fb0d1d3f
e.boganov <jenyaz862@gmail.com>
2023-02-13 18:18:56 +0300

    dep

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/maps/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/maps/'
+    : '/',
   plugins: [vue()],
 });
